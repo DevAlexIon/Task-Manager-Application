eba9c636377a92c6ed265dd971efea99d265ca94
Alexandru Ion <alexandru.a.ion@capgemini.com>
2022-10-18 07:28:46 +0000

    [68] Setup-security-headers-for-api

--- packages/server-api/src/main.ts
+++ packages/server-api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [process.env.ORIGIN, process.env.ORIGIN2],
+      credentials: true,
+    },
+  });
+  await app.listen(process.env.PORT);
+  app.use(helmet());
+  app.use(helmet.frameguard({ action: 'deny' }));
 }
 bootstrap();
