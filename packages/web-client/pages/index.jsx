00ce3ffa6380a9ce6cf5cbf8fa761bd54b579b10
Alexandru Ion <alexandru.a.ion@capgemini.com>
2022-11-08 11:28:51 +0000

    #9 Login Form

--- packages/web-client/pages/index.jsx
+++ packages/web-client/pages/index.jsx
@@ -1,30 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
-  BrowserRouter as Router, Route, Routes, Navigate,
+  BrowserRouter as Router, Route, Switch, Link,
 } from 'react-router-dom';
+import { AuthProvider } from 'react-auth-kit';
 import Login from './routes/login';
 import Register from './routes/register';
 import Home from './routes/home';
 import Health from './routes/health';
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, []);
-
   return (
-    <Router>
-      {!isLoading && (
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/health" element={<Health />} />
-          <Route path="/" element={<Navigate to="/home" />} />
-        </Routes>
-      )}
-    </Router>
+    <AuthProvider
+      authStorageType="cookie"
+      authStorageName="_auth_t"
+      authTimeStorageName="_auth_time"
+      stateStorageName="_auth_state"
+      cookieDomain={window.location.hostname}
+      cookieSecure={window.location.protocol === 'https:'}
+    >
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/health">
+              <Health />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+    </AuthProvider>
   );
 }
